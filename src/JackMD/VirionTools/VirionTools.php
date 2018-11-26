d31c3bbfb328b5065a6cb5948b3f23a0324ebbfb
JackMD <jackmtaylor.jmt@gmail.com>
2018-11-26 11:03:01 +0500

    fix spaces

--- src/JackMD/VirionTools/VirionTools.php
+++ src/JackMD/VirionTools/VirionTools.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types = 1);
+
 /**
  *  _   _ _      _           _____           _
  * | | | (_)    (_)         |_   _|         | |
@@ -7,18 +8,23 @@ declare(strict_types = 1);
  * | | | | | '__| |/ _ \| '_ \| |/ _ \ / _ \| / __|
  *  \ \_/ / | |  | | (_) | | | | | (_) | (_) | \__ \
  *   \___/|_|_|  |_|\___/|_| |_\_/\___/ \___/|_|___/
+ *
  * VirionTools, a VirionTools plugin like DevTools for PocketMine-MP.
  * Copyright (c) 2018 JackMD  < https://github.com/JackMD >
+ *
  * Discord: JackMD#3717
  * Twitter: JackMTaylor_
+ *
  * This software is distributed under "GNU General Public License v3.0".
  * This license allows you to use it and/or modify it but you are not at
  * all allowed to sell this plugin at any cost. If found doing so the
  * necessary action required would be taken.
+ *
  * VirionTools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License v3.0 for more details.
+ *
  * You should have received a copy of the GNU General Public License v3.0
  * along with this program. If not, see
  * <https://opensource.org/licenses/GPL-3.0>.
